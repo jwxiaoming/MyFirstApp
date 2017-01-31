5cf9dbd090b42aebff15344cbf7261653e44a577
jwxiaoming <LRM@gouerxindeMacBook-Pro.local>
2017-01-31 13:07:31 +0800

    None
    None

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func TouchMe(_ sender: UIButton) {
+        print("这是我的第一个App！")
+        //👇定义了通知框
+        let 通知框 = UIAlertController(title: "你好", message: "这是小明明开发的第一个App", preferredStyle: UIAlertControllerStyle.alert)
+        //👇定义了ok按钮
+        let okaction = UIAlertAction(title: "知道了", style: UIAlertActionStyle.default) { (_) in
+            print("您点击了 知道了")
+        }
+        //将ok按钮加入了通知框
+        通知框.addAction(okaction)
+        //将整套显现了出来
+        self.present(通知框, animated: true, completion: { 
+            print("显示了一个通知框")
+            }
+        )
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
